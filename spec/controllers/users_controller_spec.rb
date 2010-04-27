6c13146de88192adea53f60fe7595740b6d42ae2
Sid <sid.ravichandran@gmail.com>
2010-04-27 06:17:22 +0100

    Made some UI Changes and layout changes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -43,6 +43,7 @@ end
 describe UsersController, "handle POST /users" do
   before(:each) do
     @user = mock_model(User)
+    User.stub!(:new).and_return(@user)
     @params = valid_user_attributes
   end
 
@@ -63,10 +64,18 @@ describe UsersController, "handle POST /users" do
     response.should redirect_to(user_url("#{@user.id}"))
    end
 
+  it "should redirect the user to the show page after a successful account creation with a flash message" do
+    User.should_receive(:new).and_return(@user)
+    @user.should_receive(:save).and_return("bananahammock")
+    do_post    
+    flash[:notice].should =~ /your account was successfully created/i
+  end
+
   it "should render the new user signup page /users/new when save fails" do
     User.should_receive(:new).and_return(@user)
     @user.should_receive(:save).and_return(false)
     do_post
     response.should render_template('new')
   end
+
 end
